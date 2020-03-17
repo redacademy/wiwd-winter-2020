4d02ad417c57f4e9f7c41f4c53a66e1d491041ea
Dex <dxneufeld14@gmail.com>
2020-03-17 13:34:54 -0700

    started working on the site-footer

--- themes/women-in-webdev/footer.php
+++ themes/women-in-webdev/footer.php
@@ -11,8 +11,15 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<h3>Lorem Ipsum dolor sit amet<h3>
+						<ul class="ul-no-style">
+							<li><a>Lorem Ipsum</a></li>
+							<li><a>Ipsum Lorem </a></li>
+							<li><a>Muspi Merol</a></li>
+						<ul>	
 				</div><!-- .site-info -->
+				<div class="site-socials">
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
