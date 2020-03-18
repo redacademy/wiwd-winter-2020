c9eaa42ed5b6ad7f45d9460b7224c949ccb0420d
felixf3600 <felixf3600@gmail.com>
2020-03-18 12:30:20 -0700

    fixed front-page.php

--- themes/women-in-webdev/front-page.php
+++ themes/women-in-webdev/front-page.php
@@ -1,42 +1,26 @@
+
 <?php
 /**
- * The main template file.
+ * The template for displaying the About page.
  *
  * @package Women_In_Webdev_Theme
  */
 
 get_header(); ?>
 
+
 	<div id="primary" class="content-area">
-	 <div class="max-width-container">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main"> 
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
+       
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
-	 </div>
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
