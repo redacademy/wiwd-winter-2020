b21b20053fe2654f9ce7bcf00f49def1433918e9
felixf3600 <felixf3600@gmail.com>
2020-03-17 23:15:31 -0700

    fixed the membership.php and content-membership.php

--- themes/women-in-webdev/template-parts/content-membership.php
+++ themes/women-in-webdev/template-parts/content-membership.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying posts.
+ * Template part for displaying page content in page.php.
  *
  * @package Women_In_Webdev_Theme
  */
@@ -8,59 +8,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-<section>
-		<h3>Menbership Benefits</h3>
-		<div class= "benefits-grid">
-			<artcile class="benefits-container">
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-			<artcile class="benefits-container">				
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-			<artcile class="benefits-container">
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-			<artcile class="benefits-container">
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-			<artcile class="benefits-container">
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-			<artcile class="benefits-container">
-				<div class="benefits-image"></div>
-				<div class="benefits-description"></div>
-			</article>
-		</div>
-	</section>
-	<section class="membership">
-
-		<div class="buttons">
-			<button	class="anual"></button>
-			<button class="monthly"></button>
-		</div>
-
-	</section>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
-
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_excerpt(); ?>
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
