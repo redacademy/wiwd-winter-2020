45bcabc85b21697a429a70d09daa3d33f4c5e9c1
felixf3600 <felixf3600@gmail.com>
2020-03-19 15:08:02 -0700

    added the scholarship custom post and moved the training custom post

--- plugins/women-in-webdev-functionality/lib/functions/post-types.php
+++ plugins/women-in-webdev-functionality/lib/functions/post-types.php
@@ -70,3 +70,121 @@ function inhabitent_cpt_product()
     register_post_type('product_type', $args);
 }
 add_action('init', 'inhabitent_cpt_product', 0);
+
+function scholarships() {
+
+	$labels = array(
+		'name'                  => _x( 'scholarships', 'Post Type General Name', 'sholashii post' ),
+		'singular_name'         => _x( 'scholarship', 'Post Type Singular Name', 'sholashii post' ),
+		'menu_name'             => __( 'Scholarships', 'sholashii post' ),
+		'name_admin_bar'        => __( 'scholarship', 'sholashii post' ),
+		'archives'              => __( 'scholarships Archives', 'sholashii post' ),
+		'attributes'            => __( 'scholarship Attributes', 'sholashii post' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'sholashii post' ),
+		'all_items'             => __( 'All scholarships', 'sholashii post' ),
+		'add_new_item'          => __( 'Add New scholarship', 'sholashii post' ),
+		'add_new'               => __( 'Add new', 'sholashii post' ),
+		'new_item'              => __( 'New scholarship', 'sholashii post' ),
+		'edit_item'             => __( 'Edit scholarship', 'sholashii post' ),
+		'update_item'           => __( 'Update scholarship', 'sholashii post' ),
+		'view_item'             => __( 'View scholarship', 'sholashii post' ),
+		'view_items'            => __( 'View scholarships', 'sholashii post' ),
+		'search_items'          => __( 'Search scholarships', 'sholashii post' ),
+		'not_found'             => __( 'Not found', 'sholashii post' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'sholashii post' ),
+		'featured_image'        => __( 'Featured Image', 'sholashii post' ),
+		'set_featured_image'    => __( 'Set featured image', 'sholashii post' ),
+		'remove_featured_image' => __( 'Remove featured image', 'sholashii post' ),
+		'use_featured_image'    => __( 'Use as featured image', 'sholashii post' ),
+		'insert_into_item'      => __( 'Insert into item', 'sholashii post' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'sholashii post' ),
+		'items_list'            => __( 'Items list', 'sholashii post' ),
+		'items_list_navigation' => __( 'Items list navigation', 'sholashii post' ),
+		'filter_items_list'     => __( 'Filter items list', 'sholashii post' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'post_typescholarships',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __( 'scholarship', 'sholashii post' ),
+		'description'           => __( 'This is a for post about scholarships', 'sholashii post' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'scholarships', $args );
+
+}
+add_action( 'init', 'scholarships', 0 );
+
+// Register Custom Post Type for Training 
+function training_posts() {
+
+	$labels = array(
+		'name'                  => _x( 'Opportunities', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Opportunity', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Opportunities', 'text_domain' ),
+		'name_admin_bar'        => __( 'Opportunity', 'text_domain' ),
+		'archives'              => __( 'Opportunity Archives', 'text_domain' ),
+		'attributes'            => __( 'Opportunity Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Opportunity:', 'text_domain' ),
+		'all_items'             => __( 'All Opportunities', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Opportunity', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Opportunity', 'text_domain' ),
+		'edit_item'             => __( 'Edit Opportunity', 'text_domain' ),
+		'update_item'           => __( 'Update Opportunity', 'text_domain' ),
+		'view_item'             => __( 'View Opportunity', 'text_domain' ),
+		'view_items'            => __( 'View Opportunities', 'text_domain' ),
+		'search_items'          => __( 'Search Opportunity', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Opportunity', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Opportunity', 'text_domain' ),
+		'items_list'            => __( 'Opportunities list', 'text_domain' ),
+		'items_list_navigation' => __( 'Opportunities list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Opportunities list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Opportunity', 'text_domain' ),
+		'description'           => __( 'Creates post for training opportunities', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'revisions', 'page-attributes' ),
+		'hierarchical'          => true,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'training_posts', $args );
+
+}
+add_action( 'init', 'training_posts', 0 );
--- themes/women-in-webdev/functions.php
+++ themes/women-in-webdev/functions.php
@@ -7,11 +7,11 @@
  * @package Women_In_Webdev_Theme
  */
 
-if ( ! function_exists( 'red_starter_setup' ) ) :
+if ( ! function_exists( 'WIWD_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  */
-function red_starter_setup() {
+function WIWD_setup() {
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
@@ -36,25 +36,25 @@ function red_starter_setup() {
 	) );
 
 }
-endif; // red_starter_setup
-add_action( 'after_setup_theme', 'red_starter_setup' );
+endif; // WIWD_setup
+add_action( 'after_setup_theme', 'WIWD_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * @global int $content_width
  */
-function red_starter_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'red_starter_content_width', 640 );
+function WIWD_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'WIWD_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'red_starter_content_width', 0 );
+add_action( 'after_setup_theme', 'WIWD_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function red_starter_widgets_init() {
+function WIWD_widgets_init() {
 	register_sidebar(array(
 		'name'          => esc_html('contact'),
 		'id'            => 'contact',
@@ -73,26 +73,35 @@ function red_starter_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	));
+	register_sidebar(array(
+		'name'          => esc_html('blog'),
+		'id'            => 'blog',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	));
 
 }
-add_action( 'widgets_init', 'red_starter_widgets_init' );
+add_action( 'widgets_init', 'WIWD_widgets_init' );
 
 /**
  * Filter the stylesheet_uri to output the minified CSS file.
  */
-function red_starter_minified_css( $stylesheet_uri, $stylesheet_dir_uri ) {
+function WIWD_minified_css( $stylesheet_uri, $stylesheet_dir_uri ) {
 	if ( file_exists( get_template_directory() . '/build/css/style.min.css' ) ) {
 		$stylesheet_uri = $stylesheet_dir_uri . '/build/css/style.min.css';
 	}
 
 	return $stylesheet_uri;
 }
-add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
+add_filter( 'stylesheet_uri', 'WIWD_minified_css', 10, 2 );
 
 /**
  * Enqueue scripts and styles.
  */
-function red_starter_scripts() {
+function WIWD_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css');
 	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
@@ -102,7 +111,7 @@ function red_starter_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'red_starter_scripts' );
+add_action( 'wp_enqueue_scripts', 'WIWD_scripts' );
 
 /**
  * Custom template tags for this theme.
@@ -114,59 +123,4 @@ require get_template_directory() . '/inc/template-tags.php';
  */
 require get_template_directory() . '/inc/extras.php';
 
-
-
-// Register Custom Post Type for Training 
-function training_posts() {
-
-	$labels = array(
-		'name'                  => _x( 'Opportunities', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Opportunity', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Opportunities', 'text_domain' ),
-		'name_admin_bar'        => __( 'Opportunity', 'text_domain' ),
-		'archives'              => __( 'Opportunity Archives', 'text_domain' ),
-		'attributes'            => __( 'Opportunity Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Opportunity:', 'text_domain' ),
-		'all_items'             => __( 'All Opportunities', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Opportunity', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Opportunity', 'text_domain' ),
-		'edit_item'             => __( 'Edit Opportunity', 'text_domain' ),
-		'update_item'           => __( 'Update Opportunity', 'text_domain' ),
-		'view_item'             => __( 'View Opportunity', 'text_domain' ),
-		'view_items'            => __( 'View Opportunities', 'text_domain' ),
-		'search_items'          => __( 'Search Opportunity', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into Opportunity', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this Opportunity', 'text_domain' ),
-		'items_list'            => __( 'Opportunities list', 'text_domain' ),
-		'items_list_navigation' => __( 'Opportunities list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter Opportunities list', 'text_domain' ),
-	);
-	$args = array(
-		'label'                 => __( 'Opportunity', 'text_domain' ),
-		'description'           => __( 'Creates post for training opportunities', 'text_domain' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'revisions', 'page-attributes' ),
-		'hierarchical'          => true,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
-	);
-	register_post_type( 'training_posts', $args );
-
-}
-add_action( 'init', 'training_posts', 0 );
+// Register Custom Post Type
