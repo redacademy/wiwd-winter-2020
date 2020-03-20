60f396bda3c432cc6405b5a82c23d5a9971e39de
JadeU <jadeunderwood3@gmail.com>
2020-03-20 16:59:04 -0700

    Add start of structure to training archive page

--- themes/women-in-webdev/archive-training.php
+++ themes/women-in-webdev/archive-training.php
@@ -12,13 +12,6 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -26,9 +19,30 @@ get_header(); ?>
 					get_template_part( 'template-parts/content' );
 				?>
 
-			<?php endwhile; ?>
+			<section class="post-layout">
+					<div class="post">
+	
+						<div class="post-title"> 	
+							<?php the_title(); ?>
+						</div>
 
-			<?php the_posts_navigation(); ?>
+
+						<div class = "post-location">
+							<?php if( get_field('location') ): ?>
+								<?php the_field('location'); ?>
+							<?php endif; ?>
+						</div>
+
+						<div class="post-meta">
+								
+						</div>
+
+					</div>
+
+
+			<?php endwhile; ?>
+	</section>
+		
 
 		<?php else : ?>
 
