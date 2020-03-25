f4b450bf366dc3594ed50810b911a575cb5b4493
felixf3600 <felixf3600@gmail.com>
2020-03-25 14:17:38 -0700

    took out black borders of membership and front page

--- themes/women-in-webdev/front-page.php
+++ themes/women-in-webdev/front-page.php
@@ -12,9 +12,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main"> 
 
-       
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'template-parts/content', 'front-page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
